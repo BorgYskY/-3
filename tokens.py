0b4da144ec2eafe007be2df94b2bc1d3bdf65f00
borgysky <50081039+borgysky@users.noreply.github.com>
2023-11-15 16:25:35 +0300

    Update tokens.py

--- tokens.py
+++ tokens.py
@@ -1,6 +1,7 @@
 cur_char = ''
 
 get_char = None
+
 #digits = [x for x in "123456"]
 def get_token():
     global cur_char
@@ -8,13 +9,22 @@ def get_token():
         cur_char = get_char()
         if cur_char == '/':
             return end_tag()
-        elif cur_char == '!': 
-            skip_comment()
-        else: return start_tag()
-    elif cur_char == '>': return data()
+        elif cur_char == '!':
+            cur_char = get_char()
+            if cur_char == '-':
+                skip_comment()
+            else:
+                print(1)
+        else:
+            return start_tag()
+    elif cur_char == '>':
+        return data()
+    elif cur_char == -1:
+        return ('eof', )
 
 def start_tag():
     tag = ""
+    global cur_char
     while cur_char != '>':
         tag += cur_char
         cur_char = get_char()
@@ -22,6 +32,7 @@ def start_tag():
 
 def end_tag():
     tag = ""
+    global cur_char
     while cur_char != '>':
         cur_char = get_char()
         tag += cur_char
@@ -29,6 +40,7 @@ def end_tag():
 
 def data():
     data = ""
+    global  cur_char
     while cur_char != '<':
         cur_char = get_char()
         data += cur_char
@@ -38,3 +50,18 @@ def skip_comment():
     while cur_char != '>':
         cur_char = get_char()
     cur_char = get_char()
+
+
+if __name__ == '__main__':
+    print("Test tokens")
+    f = open("sample html.html")
+
+    def getch():
+        global cur_char
+        return f.read(1)
+
+    get_char = getch
+    cur_char = get_char()
+    for i in range(10):
+        print(get_token())
+    f.close()
