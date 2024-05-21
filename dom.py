4305e3a54463a4cfebe218ac6aff0a04a1ba0164
borgysky <50081039+borgysky@users.noreply.github.com>
2024-05-21 12:07:00 +0300

    dom finished

--- dom.py
+++ dom.py
@@ -1,34 +1,32 @@
 import tokens
 f = open("sample html.html")
 def getch():
-    tokens.cur_char
     return f.read(1)
-
 tokens.get_char = getch
 tokens.cur_char = tokens.get_char()
 temp = None
 tree = tokens.token("document", None, None)
-flag = tokens.get_token()
-# tokens.get_token()
-tree.add_child(flag)
+flag = tree
+
 while temp != 'eof':
     temp = tokens.get_token()
-    if temp != None and temp != 'eof':
-        if flag.tag != 'end' and flag.tag != 'meta' and flag.tag != 'data':
+    if temp is not None and temp != 'eof':
+        if temp.tag != 'end' and temp.tag != 'meta' and temp.tag != 'data':
             flag.add_child(temp)
             flag = temp
-        else: 
-            flag = flag.parent
+        elif temp.tag == 'meta' or temp.tag == 'data':
             flag.add_child(temp)
+        elif temp.tag == 'end':
+            if flag.tag == temp.val:
+                flag.add_child(temp)
+                flag = flag.parent 
+
 f.close()
 
-#ворует что ни попадя, начиная с головы
+def print_tree(node, level=0):
+    indent = "  " * level
+    print(f"{indent}{node}")
+    for child in node.children:
+        print_tree(child, level + 1)
 
-# class Node:
-#     def __init__(self, ):
-#         self.children = []
-#         self.variables = []
-#         self.name = ""
-#     def append(self, obj):
-#         self.children.append(obj)
-
+print_tree(tree)
